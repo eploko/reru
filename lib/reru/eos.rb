d9de7293b182c611c0b4a7d0df0632b87b9876db
Andrey Subbotin <andrey@subbotin.me>
2013-05-24 03:15:01 +0400

    Refactor the EOS event

--- lib/reru/eos.rb
+++ lib/reru/eos.rb
@@ -1,5 +1,14 @@
-require 'reru/eos_event'
+require 'reru/event'
 
 module Reru
-  EOS = Reru::EOSEvent.new.freeze
+  EOS = lambda {
+    class Tmp < Reru::Event
+      include Singleton
+  
+      def end? ; true ; end
+      def to_s ; '<Reru::EOS>' ; end
+    end
+    Tmp::instance
+  }.call.freeze
 end
+
--- lib/reru/eos_event.rb
+++
@@ -1,6 +0,0 @@
-require 'reru/event'
-
-class Reru::EOSEvent < Reru::Event
-  def eos? ; true ; end
-  def to_s ; '<Reru::EOS>' ; end
-end
