38d40c96a5038ac19f73699ee3fae0fbaec4db60
Andrey Subbotin <andrey@subbotin.me>
2013-05-26 21:45:56 +0400

    Simplify the echo example

--- examples/echo.rb
+++ examples/echo.rb
@@ -1,14 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-$LOAD_PATH << File.expand_path('../lib', __FILE__)
-
 require 'reru'
 
-reader = Reru::IO::Reader.new($stdin)
-reader.map { |line| 
-  "> Haha! You said “#{line.chomp}”.\n" 
-}.write($stdout)
-reader.select { |line| line.chomp == 'quit' }.perform { reader.stop }
-
-Reru::run
+Reru::IO::Reader.new($stdin).map { |line| "Haha! You said “#{line.chomp}”.\n" }.write($stdout)
+Reru.run
